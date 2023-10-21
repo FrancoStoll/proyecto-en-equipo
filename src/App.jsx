3e5796365a4bafa36697f9ec06fd69427404101b
FrancoStoll <francostoll@gmail.com>
2023-10-21 02:03:59 -0300

    segundo intento

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,62 @@
-import heroImg from "./assets/imagenes/technology-equipment-blue-laboratory-generated-by-ai.jpg";
-import backImg from './assets/imagenes/homemade-wooden-toy-box-colorful-craft-decoration-generated-by-ai_188544-29661.jpg'
+import otherImg from "./assets/imagenes/372662235_672981214741320_8124692078299428657_n.webp";
+import CardProducto from "./components/CardProducto";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <header>
         <nav>
-          <a href="">logo</a>
+          <a href="">Logo</a>
 
           <div>
             <ul>
-              <li>About</li>
-              <li>Products</li>
-              <li>Info</li>
-              <li>Conctact</li>
+              <li>Productos</li>
+              <li>Información</li>
+              <li>Sobre Nosotros</li>
+              <li>Contacto</li>
             </ul>
           </div>
         </nav>
       </header>
 
-      <main className="container hero">
-        <div className="left">
-          <h1>Impresiones 3D a la Medida</h1>
-          <p>
-            Descubre un mundo de posibilidades con nuestras impresiones 3D
-            personalizadas. Transforma tus ideas en objetos reales con la mejor
-            calidad y precisión. ¡Haz que tu imaginación cobre vida!
-          </p>
-          <button>Explora Ahora</button>
-        </div>
+      <main className="container hero_section">
+        <div className="hero">
+          <div className="left">
+            <h1>Impresiones 3D a la Medida</h1>
+            <p>
+              Descubre un mundo de posibilidades con nuestras impresiones 3D
+              personalizadas. Transforma tus ideas en objetos reales con la
+              mejor calidad y precisión. ¡Haz que tu imaginación cobre vida!
+            </p>
+            <button>Explora Ahora</button>
+          </div>
 
-        <div className="right">
-          <img className="first" src={heroImg} alt="imagen hero right" />
-          <img className="second" src={backImg} alt="imagen hero right" />
+          <div className="right">
+            <img className="first" src={otherImg} alt="imagen hero right" />
+            <p>Elden Ring - Malenia</p>
+          </div>
         </div>
       </main>
+
+      <section className="container">
+        <div className="productos">
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+          <CardProducto />
+        </div>
+      </section>
+
+      <Footer />
     </>
   );
 }
